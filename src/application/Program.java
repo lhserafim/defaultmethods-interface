42519d721f715c49866f578de602d2b07b6d5db7
Luiz <lhserafim@gmail.com>
2020-05-18 00:27:06 -0300

    Program criado. Sem default method

---
+++ src/application/Program.java
@@ -0,0 +1,27 @@
+package application;
+
+import services.BrazilInterestService;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        System.out.println("Amount: ");
+        double amount = sc.nextDouble();
+        System.out.println("Months: ");
+        int months = sc.nextInt();
+
+        BrazilInterestService is = new BrazilInterestService(2.0);
+        double payment = is.payment(amount,months);
+
+        System.out.println("Payment after: " + months + " months:");
+        System.out.println(String.format("%.2f",payment));
+
+        sc.close();
+
+    }
+}
